5458779d5b781a021044d8a07d83360f37e1b564
Eddie Yan <Loser777@gmail.com>
2016-11-17 23:51:34 -0800

    updates to calendar support for thermostat

--- thermo/twisted_server.py
+++ thermo/twisted_server.py
@@ -1,8 +1,10 @@
+import quickstart
 #-*- coding: utf-8 -*-
 from twisted.web import server, resource
 from twisted.internet import reactor
 from twisted.web.static import File
 import threading
+import apiclient
 import time
 import temp
 import cgi
@@ -13,6 +15,7 @@ T_STATE_LOCK = threading.Lock()
 T_VAL = -42.0
 T_DATE = "OUTATIME"
 T_TARGET = 0.0
+T_GTARGET = 0.0
 T_STATE = "OFF"
 
 def heat_on():
@@ -23,26 +26,49 @@ def heat_off():
     relay.heat.off()
     relay.fan.off()
 
+def get_gtarget():
+    try:
+        current_events = quickstart.getCurrentEvents()
+        time.sleep(5.0)
+    except Exception as e:
+        print(e)
+        print(type(e))
+        pass
+        return get_gtarget()
+    gtarget = 0.0
+    for event in current_events:
+        try:
+            cur_gtarget = float(event.split("temp=")[-1])
+            if cur_gtarget > gtarget:
+                gtarget = cur_gtarget
+        except ValueError:
+            print("Failed to parse event: " + event)
+    return gtarget
+
 def thermostat_task():
     while True:
         val = temp.read_temp()[1]
+        gtarget = get_gtarget() 
         T_STATE_LOCK.acquire()
         print("bro count: {0:d}".format(REQUEST_COUNT))
         global T_VAL
         global T_DATE
         global T_STATE
+        global T_GTARGET
+        T_GTARGET = gtarget
         T_VAL = val
         T_DATE = time.strftime("%H:%M:%S %D", time.localtime())
-        if T_STATE == "OFF" and T_VAL < T_TARGET:
+        if T_STATE == "OFF" and (T_VAL < T_TARGET or T_VAL < T_GTARGET):
             heat_on()
             T_STATE = "ON"
-        elif T_STATE == "ON" and T_VAL <= T_TARGET + 1:
+        elif T_STATE == "ON" and\
+       (T_VAL <= T_TARGET + 1 or T_VAL < T_GTARGET + 1):
             pass
         else:
             heat_off()
             T_STATE = "OFF"
         T_STATE_LOCK.release() 
-        time.sleep(0.2)
+        time.sleep(1.0)
 
 def gen_select_list(min_t, max_t):
     output = """<select name='temp'>"""
@@ -51,29 +77,31 @@ def gen_select_list(min_t, max_t):
     output += """</select>"""
     return output
 
-def gen_page(temperature, temperature_target, select, date, footnote=""):
-     return """<!DOCTYPE html> 
+def gen_page(temperature, temperature_target, gtemperature_target,\
+    select, date, state, footnote=""):
+    return """<!DOCTYPE html> 
     <html>
     <center>
     <h3>WELCOME TO SMARTY MCHOMEFACE SERVER</h3> <br>
     It is currently {0:.3f}&deg;F<br>
     Temp Set to {1:.3f}&deg;F<br>
+    Gtemp Set to {2:.3f}&deg;F<br>
     T_STATE = {5:s}
     <form method='POST'>
-    Set Temperature: {2:s} <br>
+    Set Temperature: {3:s} <br>
     <input type="submit" value="SET">
     </form>
-    {3:s} <br>
+    {4:s} <br>
     <img src='static/dat_boi.gif'>
     <br> 
-    <small>{4:s}</small>
+    <small>{6:s}</small>
     </center>
 
     <audio src='static/dat_boi.mp3' autoplay>
     </audio>
     </html>
-            """.format(temperature, temperature_target, select, date, footnote,
-                T_STATE)
+            """.format(temperature, temperature_target, gtemperature_target,\
+    select, date, state, footnote)
    
 
 class TInterface(resource.Resource):
@@ -84,12 +112,15 @@ class TInterface(resource.Resource):
         global REQUEST_COUNT 
         T_STATE_LOCK.acquire()
         temperature = T_VAL
-        temperature_target= T_TARGET
+        temperature_target = T_TARGET
+        gtemperature_target = T_GTARGET
         date = T_DATE
         REQUEST_COUNT += 1
+        state = T_STATE
         T_STATE_LOCK.release()
         select = gen_select_list(50, 69)
-        return gen_page(temperature, temperature_target, select, date)
+        return gen_page(temperature, temperature_target, gtemperature_target,\
+        select, date, state)
 
     def render_POST(self, request):
         temperature_target = float(cgi.escape(request.args['temp'][0]))
@@ -99,11 +130,14 @@ class TInterface(resource.Resource):
         T_STATE_LOCK.acquire()
         temperature = T_VAL
         T_TARGET = temperature_target
+        gtemperature_target = T_GTARGET
         date = T_DATE
         REQUEST_COUNT += 1
+        state = T_STATE
         T_STATE_LOCK.release()
         select = gen_select_list(50, 69)
-        return gen_page(temperature, temperature_target, select, date,\
+        return gen_page(temperature, temperature_target, gtemperature_target,\
+        select, date, state,\
         "Temperature just set... updates may not happen immediately.")
    
        
